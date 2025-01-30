be4525c651023566b2c5500ee3edca66ae2ed7e1
MozartOnWeb <mozartontheweb@gmail.com>
2025-01-30 02:52:16 +0000

    trying some css things

--- app/[locale]/artists/page.tsx
+++ app/[locale]/artists/page.tsx
@@ -1,3 +1,34 @@
+import { getTranslations } from "next-intl/server";
+import { Link } from "@/i18n/routing";
+import Image from "next/image";
+
+import "./page.css";
+import ArtistCard from "@/components/cards/artist";
+
 export default function Page() {
-  return <h1>Artistes Page</h1>;
+  return (
+    <div className="artists_page">
+      <div className="artists_hero">
+        <div className="artists_hero_infos">
+          <h4>Artistes</h4>
+          <p>
+            Explorez les œuvres et l'univers de nos artistes, des talents
+            uniques qui repoussent les limites de la créativité.
+          </p>
+        </div>
+        <div className="separator"></div>
+      </div>
+
+      <div className="artists_wrapper">
+        <ArtistCard />
+        <ArtistCard />
+        <ArtistCard />
+        <ArtistCard />
+        <ArtistCard />
+        <ArtistCard />
+        <ArtistCard />
+        <ArtistCard />
+      </div>
+    </div>
+  );
 }
---
+++ components/cards/artist.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Image from "next/image";
+import { Link } from "@/i18n/routing";
+
+import "./styles.css";
+
+type Props = {};
+
+const ArtistCard = (props: Props) => {
+  return (
+    <div className="artist_wrapper">
+      <Link href={"/about"}>
+        <Image
+          width={1260}
+          height={750}
+          src="https://images.pexels.com/photos/14867613/pexels-photo-14867613.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+      </Link>
+
+      <button>Kankou fofana</button>
+    </div>
+  );
+};
+
+export default ArtistCard;
