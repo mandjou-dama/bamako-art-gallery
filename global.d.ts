133f822638ef2d44ee154754ed987920db2ba20d
MozartOnWeb <mozartontheweb@gmail.com>
2025-12-17 08:40:17 +0000

    fix: standardize quotation marks in type declaration files

--- global.d.ts
+++ global.d.ts
@@ -1,12 +1,12 @@
 // Type declarations for importing CSS files
 // Allows imports like `import './page.css'` or CSS modules `import styles from './page.module.css'`
 
-declare module '*.module.css' {
+declare module "*.module.css" {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
 
-declare module '*.css' {
+declare module "*.css" {
   // For side-effect imports and simple class name objects
   const content: { readonly [key: string]: string };
   export default content;
@@ -14,27 +14,27 @@ declare module '*.css' {
 
 // Type declarations for importing image assets
 // Allows imports like `import photo from '@/public/assets/photo.jpeg'`
-declare module '*.png' {
+declare module "*.png" {
   const src: string;
   export default src;
 }
 
-declare module '*.jpg' {
+declare module "*.jpg" {
   const src: string;
   export default src;
 }
 
-declare module '*.jpeg' {
+declare module "*.jpeg" {
   const src: string;
   export default src;
 }
 
-declare module '*.gif' {
+declare module "*.gif" {
   const src: string;
   export default src;
 }
 
-declare module '*.svg' {
+declare module "*.svg" {
   const src: string;
   export default src;
 }
